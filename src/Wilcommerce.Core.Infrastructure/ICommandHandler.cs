b7d3cfcfaebfcdb7ddb1fadd33370de99bc045a8
albi.mori <albi.mori@gmail.com>
2016-01-30 18:04:11 +0100

    Domain infrastructure classes

---
+++ src/Wilcommerce.Core.Infrastructure/ICommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wilcommerce.Core.Infrastructure
+{
+    /// <summary>
+    /// Represents a generic command handler
+    /// </summary>
+    /// <typeparam name="TCommand">The type of the command to handle</typeparam>
+    public interface ICommandHandler<TCommand> where TCommand : ICommand
+    {
+        /// <summary>
+        /// Handles the specified command
+        /// </summary>
+        /// <param name="command">The command to handle</param>
+        void Handle(TCommand command);
+    }
+}
---
+++ src/Wilcommerce.Core.Infrastructure/IRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Wilcommerce.Core.Infrastructure
+{
+    /// <summary>
+    /// Represents a generic repository for the aggregate's persistence
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Add an aggregate to the repository
+        /// </summary>
+        /// <typeparam name="TModel">The aggregate's type</typeparam>
+        /// <param name="model">The aggregate to add</param>
+        void Add<TModel>(TModel model) where TModel : IAggregateRoot;
+
+        /// <summary>
+        /// Gets the aggregate based on the specified key
+        /// </summary>
+        /// <typeparam name="TModel">The aggregate's type</typeparam>
+        /// <param name="key">The key of the aggregate to search</param>
+        /// <returns>The aggregate found</returns>
+        TModel GetByKey<TModel>(Guid key) where TModel : IAggregateRoot;
+
+        /// <summary>
+        /// Async method. Gets the aggregate based on the specified key
+        /// </summary>
+        /// <typeparam name="TModel">The aggregate's type</typeparam>
+        /// <param name="key">The key of the aggregate to search</param>
+        /// <returns>The aggregate found</returns>
+        Task<TModel> GetByKeyAsync<TModel>(Guid key) where TModel : IAggregateRoot;
+
+        /// <summary>
+        /// Saves all the changes made on the aggregate
+        /// </summary>
+        void SaveChanges();
+
+        /// <summary>
+        /// Async method. Saves all the changes made on the aggregate
+        /// </summary>
+        /// <returns></returns>
+        Task SaveChangesAsyc();
+    }
+}
