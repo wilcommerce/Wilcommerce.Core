f9445908377ca1ad3f43fc368f90489b34a9d539
albi.mori <albi.mori@gmail.com>
2016-02-13 19:32:41 +0100

    Add command handler async. Edit project.json information

---
+++ src/Wilcommerce.Core.Infrastructure/ICommandHandlerAsync.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wilcommerce.Core.Infrastructure
+{
+    /// <summary>
+    /// Represents a generic command handler which handles the command using the async/await pattern
+    /// </summary>
+    /// <typeparam name="TCommand">The type of the command to handle</typeparam>
+    public interface ICommandHandlerAsync<TCommand> where TCommand : ICommand
+    {
+        /// <summary>
+        /// Async method. Handles the specified command
+        /// </summary>
+        /// <param name="command">The command to handle</param>
+        /// <returns></returns>
+        Task Handle(TCommand command);
+    }
+}
