9251f4009bda22ee83195745575bf6f621a3bd2c
albi.mori <albi.mori@gmail.com>
2016-03-03 22:42:15 +0100

    Setup Core.Common project, currency and image value object

---
+++ src/Wilcommerce.Core.Common/Domain/Models/Currency.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace Wilcommerce.Core.Common.Domain.Models
+{
+    /// <summary>
+    /// Represents a Currency
+    /// </summary>
+    public class Currency
+    {
+        /// <summary>
+        /// The currency code
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The currency amount
+        /// </summary>
+        public double Amount { get; set; }
+
+        #region Operators
+        /// <summary>
+        /// Sum two currencies
+        /// </summary>
+        /// <param name="firstCurrency">The first currency</param>
+        /// <param name="secondCurrency">The second currency</param>
+        /// <returns>The currency sum</returns>
+        public static Currency operator+(Currency firstCurrency, Currency secondCurrency)
+        {
+            if(firstCurrency == null)
+            {
+                throw new ArgumentNullException("firstCurrency");
+            }
+
+            if(firstCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            if(secondCurrency == null)
+            {
+                throw new ArgumentNullException("secondCurrency");
+            }
+
+            if (secondCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            if (firstCurrency.Code != secondCurrency.Code)
+            {
+                throw new ArgumentException("The currency must be of the same type");
+            }
+
+            double result = firstCurrency.Amount + secondCurrency.Amount;
+            if (result < 0)
+            {
+                throw new InvalidOperationException("The result must be greater than or equals to zero");
+            }
+
+            return new Currency
+            {
+                Code = firstCurrency.Code,
+                Amount = result
+            };
+        }
+
+        /// <summary>
+        /// Subtract two currencies
+        /// </summary>
+        /// <param name="firstCurrency">The first currency</param>
+        /// <param name="secondCurrency">The second currency</param>
+        /// <returns>The currency difference</returns>
+        public static Currency operator-(Currency firstCurrency, Currency secondCurrency)
+        {
+            if (firstCurrency == null)
+            {
+                throw new ArgumentNullException("firstCurrency");
+            }
+
+            if (firstCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            if (secondCurrency == null)
+            {
+                throw new ArgumentNullException("secondCurrency");
+            }
+
+            if (secondCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            if (firstCurrency.Code != secondCurrency.Code)
+            {
+                throw new ArgumentException("The currency must be of the same type");
+            }
+
+            double result = firstCurrency.Amount - secondCurrency.Amount;
+            if(result < 0)
+            {
+                throw new InvalidOperationException("The result must be greater than or equals to zero");
+            }
+
+            return new Currency
+            {
+                Code = firstCurrency.Code,
+                Amount = result
+            };
+        }
+
+        /// <summary>
+        /// Multiply two currencies
+        /// </summary>
+        /// <param name="firstCurrency">The first currency</param>
+        /// <param name="secondCurrency">The second currency</param>
+        /// <returns>The currency multiplication</returns>
+        public static Currency operator*(Currency firstCurrency, Currency secondCurrency)
+        {
+            if (firstCurrency == null)
+            {
+                throw new ArgumentNullException("firstCurrency");
+            }
+
+            if (firstCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            if (secondCurrency == null)
+            {
+                throw new ArgumentNullException("secondCurrency");
+            }
+
+            if (secondCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            double result = firstCurrency.Amount * secondCurrency.Amount;
+            if(result < 0)
+            {
+                throw new InvalidOperationException("The result must be greater than or equals to zero");
+            }
+
+            return new Currency
+            {
+                Code = firstCurrency.Code,
+                Amount = result
+            };
+        }
+
+        /// <summary>
+        /// Divide two currencies
+        /// </summary>
+        /// <param name="firstCurrency">The first currency</param>
+        /// <param name="secondCurrency">The second currency</param>
+        /// <returns>The currency division</returns>
+        public static Currency operator/(Currency firstCurrency, Currency secondCurrency)
+        {
+            if (firstCurrency == null)
+            {
+                throw new ArgumentNullException("firstCurrency");
+            }
+
+            if (firstCurrency.Amount < 0)
+            {
+                throw new ArgumentException("The currency must be greater than or equal to zero");
+            }
+
+            if (secondCurrency == null)
+            {
+                throw new ArgumentNullException("secondCurrency");
+            }
+
+            if (secondCurrency.Amount <= 0)
+            {
+                throw new ArgumentException("The currency must be greater than to zero");
+            }
+
+            double result = firstCurrency.Amount / secondCurrency.Amount;
+            if (result < 0)
+            {
+                throw new InvalidOperationException("The result must be greater than or equals to zero");
+            }
+
+            return new Currency
+            {
+                Code = firstCurrency.Code,
+                Amount = result
+            };
+        }
+        #endregion
+    }
+}
---
+++ src/Wilcommerce.Core.Common/Domain/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace Wilcommerce.Core.Common.Domain.Models
+{
+    public class Image
+    {
+        public byte[] Data { get; set; }
+
+        public string MimeType { get; set; }
+    }
+}
