4f5d50874ec36d843037bb9393007b12a1dbf843
albi.mori <albi.mori@gmail.com>
2016-08-14 19:38:27 +0200

    update global.json, add IEventBus interface

---
+++ src/Wilcommerce.Core.Infrastructure/IEventBus.cs
@@ -0,0 +1,22 @@
+﻿namespace Wilcommerce.Core.Infrastructure
+{
+    /// <summary>
+    /// Represents a generic event bus
+    /// </summary>
+    public interface IEventBus
+    {
+        /// <summary>
+        /// Raise the specified event
+        /// </summary>
+        /// <typeparam name="TEvent">The event's type</typeparam>
+        /// <param name="event">The event to raise</param>
+        void RaiseEvent<TEvent>(TEvent @event) where TEvent : DomainEvent;
+
+        /// <summary>
+        /// Register the handler for the event
+        /// </summary>
+        /// <typeparam name="TEvent">The event's type</typeparam>
+        /// <typeparam name="THandler">The handler's type</typeparam>
+        void RegisterHandler<TEvent, THandler>() where TEvent : DomainEvent where THandler : IHandleEvent<TEvent>;
+    }
+}
