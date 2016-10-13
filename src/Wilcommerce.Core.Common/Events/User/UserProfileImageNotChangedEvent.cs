bedfd74688cc1befc34fcdcaaeb90578e51d031e
albi.mori <albi.mori@gmail.com>
2016-10-13 21:44:37 +0200

    remove unnecessary using

--- src/Wilcommerce.Core.Common/Events/User/UserProfileImageNotChangedEvent.cs
+++ src/Wilcommerce.Core.Common/Events/User/UserProfileImageNotChangedEvent.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Core.Common.Events.User
