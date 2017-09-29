8e27b8e0144a5f173d2d9b99c8cb171a05b3447c
albi.mori <albi.mori@gmail.com>
2017-09-29 09:39:48 +0200

    remove events from common database, change api for the event store

--- src/Wilcommerce.Core.Infrastructure/IEventStore.cs
+++ src/Wilcommerce.Core.Infrastructure/IEventStore.cs
@@ -1,4 +1,7 @@
-﻿namespace Wilcommerce.Core.Infrastructure
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wilcommerce.Core.Infrastructure
 {
     /// <summary>
     /// Represents a generic event store
@@ -11,5 +14,41 @@
         /// <typeparam name="TEvent">The type of the event to save</typeparam>
         /// <param name="event">The event to save</param>
         void Save<TEvent>(TEvent @event) where TEvent : DomainEvent;
+
+        /// <summary>
+        /// Find all the events of the specified type
+        /// </summary>
+        /// <typeparam name="TEvent">The event's type to search for</typeparam>
+        /// <param name="timestamp">The timestamp by which filters the event's list</param>
+        /// <returns>A list of events</returns>
+        IEnumerable<TEvent> Find<TEvent>(DateTime timestamp) where TEvent : DomainEvent;
+
+        /// <summary>
+        /// Find all the events of the specified type
+        /// </summary>
+        /// <typeparam name="TEvent">The event's type to search for</typeparam>
+        /// <param name="entityType">The entity type which fired the events</param>
+        /// <param name="timestamp">The timestamp by which filters the event's list</param>
+        /// <returns>A list of events</returns>
+        IEnumerable<TEvent> Find<TEvent>(string entityType, DateTime timestamp) where TEvent : DomainEvent;
+
+        /// <summary>
+        /// Find all the events of the specified type
+        /// </summary>
+        /// <typeparam name="TEvent">The event's type to search for</typeparam>
+        /// <param name="entityType">The entity type which fired the events</param>
+        /// <param name="entityId">The id of the entity which fired the events</param>
+        /// <param name="timestamp">The timestamp by which filters the event's list</param>
+        /// <returns>A list of events</returns>
+        IEnumerable<TEvent> Find<TEvent>(string entityType, Guid entityId, DateTime timestamp) where TEvent : DomainEvent;
+
+        /// <summary>
+        /// Find all the occured events for the specified entity
+        /// </summary>
+        /// <param name="entityType">The entity type which fired the events</param>
+        /// <param name="entityId">The id of the entity which fired the events</param>
+        /// <param name="timestamp">The timestamp by which filters the event's list</param>
+        /// <returns>The list of occured events</returns>
+        IEnumerable<DomainEvent> FindAll(string entityType, Guid entityId, DateTime timestamp);
     }
 }
