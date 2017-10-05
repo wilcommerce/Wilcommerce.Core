17303e24f4dbea8b7eb286ed4efc4a3798f4ab6a
albi.mori <albi.mori@gmail.com>
2017-10-05 15:38:24 +0200

    add comments to settings commands

--- src/Wilcommerce.Core.Common/Commands/GeneralSettings/DisableSiteCommand.cs
+++ src/Wilcommerce.Core.Common/Commands/GeneralSettings/DisableSiteCommand.cs
@@ -3,12 +3,26 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Core.Common.Commands.GeneralSettings
 {
+    /// <summary>
+    /// Disable the site
+    /// </summary>
     public class DisableSiteCommand : ICommand
     {
+        /// <summary>
+        /// Get the settings id
+        /// </summary>
         public Guid SettingsId { get; }
 
+        /// <summary>
+        /// Get the message to show
+        /// </summary>
         public string Message { get; }
 
+        /// <summary>
+        /// Construct the command
+        /// </summary>
+        /// <param name="settingsId">The settings id</param>
+        /// <param name="message">The message to show</param>
         public DisableSiteCommand(Guid settingsId, string message)
         {
             SettingsId = settingsId;
--- src/Wilcommerce.Core.Common/Commands/GeneralSettings/EnableSiteCommand.cs
+++ src/Wilcommerce.Core.Common/Commands/GeneralSettings/EnableSiteCommand.cs
@@ -3,13 +3,23 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Core.Common.Commands.GeneralSettings
 {
+    /// <summary>
+    /// Enable the site
+    /// </summary>
     public class EnableSiteCommand : ICommand
     {
+        /// <summary>
+        /// Get the settings id
+        /// </summary>
         public Guid SettingsId { get; }
 
+        /// <summary>
+        /// Construct the command
+        /// </summary>
+        /// <param name="settingsId">The settings id</param>
         public EnableSiteCommand(Guid settingsId)
         {
-
+            SettingsId = settingsId;
         }
     }
 }
